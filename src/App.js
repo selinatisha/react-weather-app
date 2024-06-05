87da4f6cc3dd27a2db6db81c2ab314615933da0b
selinatisha <selinadieye@gmail.com>
2024-06-05 15:17:17 +0200

    Added Form component
    input and button was added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from "./logo.svg";
+import React from "react";
+
+import Form from "./Form";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Hello world!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My awesome weather app</h1>
+      <h2>Built with React ⚛️</h2>
+      <body>
+        <div className="square">
+          <Form />
+        </div>
+        <script src="index.js"></script>
+      </body>
     </div>
   );
 }
---
+++ src/Form.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <form>
+      <input type="search"></input>
+      <button type="submit" value="Search"></button>
+    </form>
+  );
+}
