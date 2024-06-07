bae5c98485ad794a375a93e8ea7082637e1b5ad5
selinatisha <selinadieye@gmail.com>
2024-06-07 11:29:47 +0200

    Added Current Component

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,36 @@
 import React from "react";
-
 export default function Form() {
   return (
-    <form>
-      <input type="search"></input>
-      <button type="submit" value="Search"></button>
-    </form>
+    <div className="Form">
+      <div className="row g-3 align-items-center">
+        <div className="col-auto">
+          <label for="inputPassword6" className="col-form-label"></label>
+        </div>
+        <div className="col-auto">
+          <input
+            placeholder="Enter City"
+            type="text"
+            id="inputCity"
+            className="form-control"
+            aria-describedby="passwordHelpInline"
+          />
+        </div>
+        <div className="col-auto">
+          <span id="passwordHelpInline" className="form-text">
+            {" "}
+          </span>
+        </div>
+      </div>
+      <button type="button" className="btn btn-danger" id="search-button">
+        Search
+      </button>
+      <button
+        type="button"
+        className="btn btn-info"
+        id="current-location-button"
+      >
+        📍{" "}
+      </button>
+    </div>
   );
 }
