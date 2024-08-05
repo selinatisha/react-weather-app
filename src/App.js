d8b2fa6556f74aa09ac42a702bc4d56f09208d07
selinatisha <selinadieye@gmail.com>
2024-08-05 13:46:02 +0200

    header addition

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ import "./Forecast.css";
 function App() {
   return (
     <div className="App">
+      <div className="header">
+        <h1>My React Weather App</h1>
+        <h2>Search the weather for any city or your current location</h2>
+      </div>
       <Form />
       <script src="index.js"></script>
     </div>
